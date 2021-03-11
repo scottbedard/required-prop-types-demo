2ada5c9bb784e3328ad3a5b92caea2e1cb81b29e
Scott Bedard <scottbedard90@gmail.com>
2021-03-11 10:21:50 -0600

    simplifying a bit

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import App from './App.vue'
 import CompositionApi, { defineComponent, ExtractPropTypes } from '@vue/composition-api'
-import Vue, { VueConstructor } from 'vue'
+import Vue from 'vue'
 
 Vue.use(CompositionApi);
 
 // helper to maintain type coverage while mounting root components
-function renderRootComponent<T extends VueConstructor>(
+function renderRootComponent<T extends ReturnType<typeof defineComponent>>(
   component: T,
-  props: T extends ReturnType<typeof defineComponent>
-    ? ExtractPropTypes<NonNullable<T['props']>>
-    : Record<string, any>
+  props: ExtractPropTypes<NonNullable<T['props']>>
 ) {
   new Vue({
     render: h => h(component, { props })
